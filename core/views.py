18bd237a72e5798a5f4c5baaa116c427d54d0363
Wicklife Oguda <ogudawicklife1@gmail.com>
2024-09-04 18:58:24 +0300

    Update home view to query db

--- core/views.py
+++ core/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
+from quiz.models import Topic, Question
+from django.db.models import Q
+from django.http import HttpResponse, HttpRequest
 
 
-def home(request):
+def home(request: HttpRequest) -> HttpResponse:
+    questions = Question.objects.all()
+    topics = Topic.objects.all()
+    active_topic = request.GET.get("topic", "")
+
+    if active_topic:
+        questions = questions.filter(topic__name=active_topic)
+
+    search = request.GET.get("search", "")
+    if search:
+        questions = questions.filter(
+            Q(header__icontains=search) | Q(description__icontains=search)
+        )
+    context = {
+        "questions": questions,
+        "topics": topics,
+        "active_topic": active_topic,
+    }
+
     return render(
         request=request,
         template_name="core/homepage.html",
+        context=context,
     )
