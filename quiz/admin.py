a8c10c666ec6e7b53363f7c838820262e4603f13
Wicklife Oguda <ogudawicklife1@gmail.com>
2024-09-04 18:51:14 +0300

    Implement Topic and Question models, admin registration and apply migrations

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from quiz.models import Topic, Question
 
-# Register your models here.
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    readonly_fields = ("id",)
+    ordering = ("name",)
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    readonly_fields = ("id",)
+    ordering = ("topic__name",)
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Topic(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    header = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    answer = models.TextField()
+    topic = models.ForeignKey(Topic, related_name="questions", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.header
